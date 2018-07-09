acce9096651ef91700d07570ea447a25c4f4362c
Omar De Los Santos <omarnyte@gmail.com>
2018-07-09 16:58:38 -0700

    Delegate response output to Client class

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,13 +1,10 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.lang.Thread;
 
 public class Server {
-  private Socket client;
-  private BufferedReader in;
-  private PrintWriter out; 
   private int port; 
   private Router router;
-  private ServerSocket server;
 
   Server(int port, Router router) {
     this.port = port;
@@ -18,22 +15,10 @@ public class Server {
     System.out.println("Listening on port " + this.port + ":");
 
     try {
-      this.server = new ServerSocket(port);
+      ServerSocket server = new ServerSocket(port);
       while (true) {
-        initiateClient();
-
-        String requestString = stringifyRequest();
-
-        if (requestString == "") {
-          continue;
-        }
-        
-        Request request = parseRequest(requestString);
-        Response response = getResponse(request);
- 
-        out.print(response.toString());
-
-        closeConnection();
+        Socket clientSocket = server.accept();
+        (new Thread(new Client(clientSocket, router))).start();
       } 
     }
     catch (Exception e) {
@@ -42,42 +27,6 @@ public class Server {
     }
   }
 
-  private void initiateClient() throws IOException {
-    this.client = this.server.accept();
-    this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-    this.out = new PrintWriter(client.getOutputStream());
-  }
-
-  private String stringifyRequest() throws IOException {
-    String requestString = "";
-    String line;
-    while ((line = this.in.readLine()) != null) {
-      if (line.length() == 0) {
-        break;
-      } else {
-        requestString += line;
-      }
-    }
-
-    return requestString;
-  }
-
-  private Request parseRequest(String requestString) {
-    RequestParser requestParser = new RequestParser(requestString);
-    Request request = requestParser.generateRequest();
-    return request;
-  }
-
-  private Response getResponse(Request request) {
-    Handler handler = this.router.getHandler(request.getURI());
-    Response response = handler.generateResponse(request);
-    return response;
-  }
-
-  private void closeConnection() throws IOException {
-    this.out.close(); 
-    this.in.close(); 
-    this.client.close(); 
-  }
+  
 
 }
