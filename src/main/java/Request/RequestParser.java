43634e7a935e2dae4e49c33d692672e155df0f81
Omar De Los Santos <omarnyte@gmail.com>
2018-07-11 09:55:22 -0700

    Move #parseRequestLine into #generateRequest

--- src/main/java/Request/RequestParser.java
+++ src/main/java/Request/RequestParser.java
@@ -7,21 +7,21 @@ public class RequestParser {
 
   RequestParser(String requestString) {
     this.splitRequest = requestString.split("\r\n");
-    parseRequestLine();
   }
-
-  private void parseRequestLine() {
-    String requestLineString = this.splitRequest[0];
-    this.requestLine = new RequestLine(requestLineString);
-  }
-
+  
   public Request generateRequest() {
+    parseRequestLine();
     Request request = new Request.Builder()
                                  .method(this.requestLine.getMethod())
                                  .uri(this.requestLine.getURI())
                                  .version(this.requestLine.getHTTPVersion())
                                  .build();
     return request;
+  }
+
+  private void parseRequestLine() {
+    String requestLineString = this.splitRequest[0];
+    this.requestLine = new RequestLine(requestLineString);
   }
 
 }
