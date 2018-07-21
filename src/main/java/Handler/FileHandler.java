6281c1de7d8c7625f67a55ceae11ea9cb771f157
Omar De Los Santos <omar.dlsg@gmail.com>
2018-07-20 17:28:10 -0700

    FileHandler builds and returns response with correct status code, phrase, and message body

---
+++ src/main/java/Handler/FileHandler.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader; 
+import java.io.File; 
+import java.io.FileReader; 
+import java.io.IOException; 
+ 
+public class FileHandler implements Handler { 
+  String directoryPath; 
+  String filePath; 
+  Request request; 
+ 
+  public FileHandler(String directoryPath) { 
+    this.directoryPath = directoryPath; 
+  } 
+   
+  public Response generateResponse(Request request) { 
+    this.request = request; 
+     
+    switch (request.getMethod()) { 
+      case "GET":  
+        return buildGETResponse(); 
+      default:  
+        return new Response.Builder(HttpStatusCode.METHOD_NOT_ALLOWED) 
+                           .build(); 
+    } 
+  } 
+ 
+  private Response buildGETResponse() { 
+    int statusCode = determineStatusCode();
+    String messageBody = createMessageBody(statusCode);
+     
+    return new Response.Builder(statusCode) 
+                       .messageBody(messageBody) 
+                       .build(); 
+  } 
+ 
+  private int determineStatusCode() { 
+    File file = createFileFromURI(); 
+    return file.exists() ? HttpStatusCode.OK : HttpStatusCode.NOT_FOUND; 
+  } 
+ 
+  private File createFileFromURI() { 
+    this.filePath = this.directoryPath + request.getURI(); 
+    return new File(this.filePath); 
+  } 
+  
+  private String createMessageBody(int statusCode) {
+    String emptyMessageBody = "";
+    return (statusCode == HttpStatusCode.OK) ? stringifyFileContent() : emptyMessageBody;
+  }
+ 
+  private String stringifyFileContent() { 
+    String content = ""; 
+     
+    try { 
+      BufferedReader reader = new BufferedReader(new FileReader(this.filePath)); 
+      String line; 
+      while ((line = reader.readLine()) != null) { 
+        content += line + "\n"; 
+      } 
+    } catch(IOException e) { 
+      e.printStackTrace(); 
+    } 
+ 
+    return content; 
+  } 
+ 
+}
