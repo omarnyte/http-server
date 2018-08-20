a6f607c82b12aaf2ea81aed27a484be4efceb13b
Omar De Los Santos <omar.dlsg@gmail.com>
2018-08-20 11:31:11 -0700

    Throw BadRequestException from RequestLine when passed in bad request line

--- src/test/java/Request/RequestLineTest.java
+++ src/test/java/Request/RequestLineTest.java
@@ -1,8 +1,32 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class RequestLineTest {
-  private RequestLine requestLine = new RequestLine("GET /uri/path HTTP/1.1");
+  private final static String METHOD = "GET";
+  private final static String URI = "/uri/path";
+  private final static String VERSION = "HTTP/1.1";
+
+  private static RequestLine requestLine;
+  
+  @BeforeClass
+  public static void setUp() throws BadRequestException {
+    String requestLineString = String.format("%s %s %s", METHOD, URI, VERSION);
+    requestLine = new RequestLine(requestLineString);
+  }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none(); 
+  
+  @Test 
+  public void throwsBadRequestException() throws BadRequestException {
+    String requestLineString = "GET\r\n";
+    
+    thrown.expect(BadRequestException.class);
+    RequestLine requestLine = new RequestLine(requestLineString);
+  }
   
   @Test 
   public void getsMethod() {
