7a17348150fc84c7e9cdf4672ad17f49d488e645
Omar De Los Santos <omar.dlsg@gmail.com>
2018-09-14 13:21:59 -0700

    Support authentication when responding to CORS request

--- src/main/java/CorsMiddleware.java
+++ src/main/java/CorsMiddleware.java
@@ -3,14 +3,23 @@ import java.util.HashMap;
 public class CorsMiddleware extends Middleware {
 
   public Response applyMiddleware(Response originalResponse) {
-    HashMap<String, String> updatedHeaders = originalResponse.getHeaders();
-    updatedHeaders.put(MessageHeader.AC_ALLOW_ORIGIN, "*");
-    Response updatedResponse = new Response.Builder(originalResponse.getStatusCode()) 
-                       .headers(updatedHeaders)
-                       .messageBody(originalResponse.getMessageBody())
-                       .build();
-
+    HashMap<String, String> updatedHeaders = addCorsHeaders(originalResponse.getHeaders());
+    Response updatedResponse = updateResponseWithHeaders(originalResponse, updatedHeaders);
     return checkNext(updatedResponse);
+  }
+
+  private HashMap<String, String> addCorsHeaders(HashMap<String, String> originalHeaders) {
+    HashMap<String, String> updatedHeaders = originalHeaders;
+    updatedHeaders.put(MessageHeader.AC_ALLOW_ORIGIN, "http://localhost:8888");
+    updatedHeaders.put(MessageHeader.AC_ALLOW_CREDENTIALS, "true");
+    return updatedHeaders;
+  }
+
+  private Response updateResponseWithHeaders(Response response, HashMap<String, String> headers) {
+    return new Response.Builder(response.getStatusCode()) 
+                       .headers(headers)
+                       .messageBody(response.getMessageBody())
+                       .build();
   }
 
 }
